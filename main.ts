71cf5c3689fa48cb9c9ee6ad62ea93e6b0a1902c
Jason Warren <contact.jwarren@gmail.com>
2025-01-16 11:48:43 +0000

    feat: :card_file_box: connect to AuraDB from server

--- main.ts
+++ main.ts
@@ -1,121 +1,58 @@
 import { Application } from "jsr:@oak/oak/application";
-import { Router } from "jsr:@oak/oak/router";
 import { oakCors } from "https://deno.land/x/cors@v1.2.2/mod.ts";
+import * as dotenv from "jsr:@std/dotenv";
+import router from "./routes/main.ts";
+import neo4j from "neo4j";
+import { toNumber } from "../../../Library/Caches/deno/npm/registry.npmjs.org/neo4j-driver-core/5.27.0/types/integer.d.ts";
+
+console.groupCollapsed("=== Loading ENV ===");
+await dotenv.load({export: true});
+console.groupEnd();
 
 const app = new Application();
-const router = new Router();
 const port: number = 8080;
 
-router.get("/", (ctx) => {
-  ctx.response.body = `<!DOCTYPE html>
-    <html>
-      <head>
-        <title>LIFT Server</title>
-      <head>
-      <body>
-        <header>
-          <h1>LIFT Server?</h1>
-          <pre>LIFT Server!</pre>
-        </header>
-        <hr/>
-        <nav>
-          <ul>
-            <li><a href="/who">WHO??¿???</a></li>
-          </ul>
-        </nav>
-        <hr/>
-        <main>
-          <section>
-            <h2>Tech Stack</h2>
+app.use( oakCors({ 
+  origin: "http://localhost:8000" 
+}));
 
-            <div>
-              <h3>Running on...</h3>
-              <ul>
-                <li><a href="https://deno.com/">Deno</a></li>
-                <li><a href="https://oakserver.org/">Oak</a></li>
-              </ul>
-            </div>
+// Database
+(async () => {
+  console.groupCollapsed("=== Defining ENV ===");
+  const URI:string = Deno.env.get("NEO4J_URI") ?? "";
+  const USER:string = Deno.env.get("NEO4J_USERNAME") ?? "";
+  const PASSWORD:string = Deno.env.get("NEO4J_PASSWORD") ?? "";
 
-            <div>
-              <h3>Will Run on...</h3>
-              <ul>
-                <li><a href="https://neo4j.com/">neo4j</a></li>
-              </ul>
-            </div>
-          <section>
-          <hr/>
-          <section>
-            <h2>Links</h2>
+  console.log(`URI: ${URI}`);
+  console.log(`USER: ${USER}`);
+  console.log(`PASSWORD: ${PASSWORD}`);
+  console.groupEnd();
 
-            <ul>
-              <li><a href="https://github.com/foundersandcoders/LIFT-backend">Back End Repo</a></li>
-              <li><a href="https://console-preview.neo4j.io/tools/query">AuraDB Instance</a></li>
-            </ul>
-          </section>
-        </main>
-        <hr/>
-        <footer>
-          <pre>© Alex & the Pig 2025</pre>
-        </footer>
-      </body>
-    </html>
-  `;
-});
+  let driver:neo4j.Driver;
 
-router.get("/who", (ctx) => {
-  ctx.response.body = `<!DOCTYPE html>
-    <html>
-      <head>
-        <title>LIFT Server</title>
-      <head>
-      <body>
-        <header>
-          <h1>LIFT Server?</h1>
-          <pre>LIFT Server!</pre>
-        </header>
-        <hr/>
-        <nav>
-          <ul>
-            <li><a href="/">WHAT????⸮</a></li>
-          </ul>
-        </nav>
-        <hr/>
-        <main>
-          <section>
-            <h2>Who Made This?</h2>
-            <div>
-              <h3><a href="https://github.com/AlexVOiceover">Alex Rodriguez</a></h3>
-              <ul>
-                <li>Tall</li>
-                <li>Good bone structure</li>
-              </ul>
-            </div>
-            <div>
-              <h3><a href="https://github.com/JasonWarrenUK">A Pig</a></h3>
-              <ul>
-                <li>Enthusiastic</li>
-              </ul>
-            </div>
-          <section>
-        </main>
-        <hr/>
-        <footer>
-          <pre>© Alex & the Pig 2025</pre>
-        </footer>
-      </body>
-    </html>
-  `;
-});
+  console.groupCollapsed("=== Attempting DB Connection ===");
+  try {
+    driver = neo4j.driver(URI, neo4j.auth.basic(USER, PASSWORD));
+    const serverInfo = await driver.getServerInfo();
+    console.log('Connection established')
+    console.log(serverInfo)
+    console.groupCollapsed("=== Closing DB ===");
+    driver.close();
+    console.groupEnd();
+  } catch(err) {
+    console.log(`Connection error\n${err}\nCause: ${err.cause}`)
+  }
+  console.groupEnd();
+})();
 
-app.use( /* CORS module goez hir */
-  oakCors({ 
-    origin: "http://localhost:8000" 
-  })
-);
 
+// Router
+console.groupCollapsed("=== Calling Routes ===");
 app.use(router.routes());
 app.use(router.allowedMethods());
+console.groupEnd();
 
+console.groupCollapsed("=== Starting Server ===");
 app.listen({port});
-
 console.log(`Server is running on port ${port}`);
+console.groupEnd();
---
+++ routes/main.ts
@@ -0,0 +1,35 @@
+import { Router } from "jsr:@oak/oak/router";
+import neo4j from "neo4j";
+import * as stuff from "../piles/html.tsx";
+
+// Router
+const router = new Router();
+
+router.get("/", (ctx) => {
+  ctx.response.body = (stuff.pageStart
+    + stuff.header
+    + stuff.nav
+    + stuff.home
+    + stuff.footer
+  + stuff.pageEnd);
+});
+
+router.get("/who", (ctx) => {
+  ctx.response.body = (stuff.pageStart
+    + stuff.header
+    + stuff.nav
+    + stuff.who
+    + stuff.footer
+  + stuff.pageEnd);
+});
+
+router.get("/neo4j", (ctx) => {
+  ctx.response.body = (stuff.pageStart
+    + stuff.header
+    + stuff.nav
+    + stuff.neo4j
+    + stuff.footer
+  + stuff.pageEnd);
+});
+
+export default router;
