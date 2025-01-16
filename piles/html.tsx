c1cf3a081dd6782ea9c854a5e93b1906e0b55192
Jason Warren <contact.jwarren@gmail.com>
2025-01-16 11:48:43 +0000

    feat: :card_file_box: connect to AuraDB from server

---
+++ piles/html.tsx
@@ -0,0 +1,77 @@
+// Doc Setup
+export const pageStart = `<!DOCTYPE html>
+  <html>
+  <head>
+    <title>LIFT Server</title>
+  <head>
+<body>`;
+export const pageEnd = `</body>
+</html>`;
+
+// Structure
+export const header = `<header>
+  <h1>LIFT Server?</h1>
+  <pre>LIFT Server!</pre>
+</header>`;
+export const nav = `<nav>
+  <ul>
+    <li><a href="/who">WHO??¿???</a></li>
+    <li><a href="/neo4j">knee? yo! forge.</a></li>
+  </ul>
+</nav>`;
+export const footer = `<footer>
+  <pre>© Alex & the Pig 2025</pre>
+</footer>`;
+
+// Pages
+export const home = `<main>
+  <section>
+    <h2>Tech Stack</h2>
+    <div>
+      <h3>Running on...</h3>
+      <ul>
+        <li><a href="https://deno.com/">Deno</a></li>
+        <li><a href="https://oakserver.org/">Oak</a></li>
+      </ul>
+    </div>
+    <div>
+      <h3>Will Run on...</h3>
+      <ul>
+        <li><a href="https://neo4j.com/">neo4j</a></li>
+      </ul>
+    </div>
+  <section>
+  <hr/>
+  <section>
+    <h2>Links</h2>
+    <ul>
+      <li><a href="https://github.com/foundersandcoders/LIFT-backend">Back End Repo</a></li>
+      <li><a href="https://console-preview.neo4j.io/tools/query">AuraDB Instance</a></li>
+    </ul>
+  </section>
+</main>`;
+export const who = `<main>
+  <section>
+    <h2>Who Made This?</h2>
+    <div>
+      <h3><a href="https://github.com/AlexVOiceover">Alex Rodriguez</a></h3>
+      <ul>
+        <li>Tall</li>
+        <li>Good bone structure</li>
+      </ul>
+    </div>
+    <div>
+      <h3><a href="https://github.com/JasonWarrenUK">A Pig</a></h3>
+      <ul>
+        <li>Enthusiastic</li>
+      </ul>
+    </div>
+  <section>
+</main>`;
+export const neo4j = `<main>
+  <h2>An Example of the Thing People Keep Telling Me is "Too Complicated"</h2>
+  
+  <ul>
+    <li>will be here</li>
+  </ul>
+</main>`;
