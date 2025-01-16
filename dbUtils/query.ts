833e2b591598ef6ff4f87ba468d2e5ca6e7c3328
Jason Warren <contact.jwarren@gmail.com>
2025-01-16 14:53:54 +0000

    feat: :card_file_box: write to db from server

--- dbUtils/query.ts
+++ dbUtils/query.ts
@@ -1,88 +1,63 @@
-import { Application } from "jsr:@oak/oak/application";
-import { oakCors } from "https://deno.land/x/cors@v1.2.2/mod.ts";
-import * as dotenv from "jsr:@std/dotenv";
-import router from "./routes/main.ts";
 import neo4j from "neo4j";
-
-await dotenv.load({export: true});
-
-const app = new Application();
-const port: number = 8080;
-
-app.use( oakCors({ 
-  origin: "http://localhost:8000" 
-}));
 
 (async () => {
   const URI:string = Deno.env.get("NEO4J_URI") ?? "";
   const USER:string = Deno.env.get("NEO4J_USERNAME") ?? "";
   const PASSWORD:string = Deno.env.get("NEO4J_PASSWORD") ?? "";
-
   let driver, result;
 
   const people = [
-    { 
-      name: "Alex",
+    { name: "Alex",
       worksFor: ["Dan"],
       worksWith: ["Jason"],
       species: "Human"
     },
-    { 
-      name: "Alphonso",
+    { name: "Alphonso",
       worksFor: ["Biela", "Islington Council"],
       worksWith: ["Nich"],
       species: "Human"
     },
-    { 
-      name: "Anna",
+    { name: "Anna",
       worksFor: ["Dan"],
       worksWith: ["Jess"],
       species: "Human"
     },
-    { 
-      name: "Biela",
+    { name: "Biela",
       worksFor: [],
       worksWith: ["Jess"],
       species: "Dog"
     },
-    { 
-      name: "Dan",
+    { name: "Dan",
       worksFor: [],
       worksWith: [],
       species: "Human"
     },
-    { 
-      name: "Jack",
+    { name: "Jack",
       worksFor: ["Dan"],
       worksWith: ["Max"],
       species: "Human"
     },
-    { 
-      name: "Jason",
+    { name: "Jason",
       worksFor: ["Dan"],
       worksWith: ["Alex"],
       species: "Goblin"
     },
-    { 
-      name: "Jess",
+    { name: "Jess",
       worksFor: ["Dan"],
       worksWith: ["Biela", "Anna"],
       species: "Human"
     },
-    { 
-      name: "Max",
+    { name: "Max",
       worksFor: ["Dan"],
       worksWith: ["Jack"],
       species: "Human"
     },
-    { 
-      name: "Nich",
+    { name: "Nich",
       worksFor: ["Dan"],
       worksWith: [],
       species: "Human"
     },
-    { 
-      name: "Shaughn",
+    { name: "Shaughn",
       worksFor: ["Dan"],
       worksWith: [],
       species: "Human"
@@ -92,7 +67,6 @@ app.use( oakCors({
   try {
     driver = neo4j.driver(URI,  neo4j.auth.basic(USER, PASSWORD))
     await driver.verifyConnectivity()
-    console.log("DB On")
   } catch(err) {
     console.log(`Connection error\n${err}\nCause: ${err.cause}`)
     await driver.close()
@@ -122,7 +96,7 @@ app.use( oakCors({
     if(person.worksWith != undefined) {
       await driver.executeQuery(`
         MATCH (p:Person {name: $person.name})
-        UNWIND $person.worksWith AS colleagueName
+        UNWIND $person.worksFor AS colleagueName
         MATCH (colleague:Person {name: colleagueName})
         MERGE (p)-[:WORKS_WITH]->(colleague)
         `, { person: person },
@@ -131,15 +105,19 @@ app.use( oakCors({
     }
   }
 
+  
   result = await driver.executeQuery(`
-    MATCH (p:Person)-[r:WORKS_WITH]-(c:Person)
-    RETURN p, r, c
-    `,
+    MATCH (p:Person)-[:WORKS_FOR]-(boss:Person)
+    WHERE boss.species = $species
+    RETURN boss
+    `, { species: "Dog" },
     { database: 'neo4j' }
   )
-
-  for(const person of result.records) {
-    console.log(person.get('p', 'r', 'c'))
+/* 
+  // Loop through results and do something with them
+  for(let person of result.records) {
+    // `person.friend` is an object of type `Node`
+    console.log(person.get('friend'))
   }
 
   // Summary information
@@ -147,14 +125,7 @@ app.use( oakCors({
     `The query \`${result.summary.query.text}\` ` +
     `returned ${result.records.length} records ` +
     `in ${result.summary.resultAvailableAfter} ms.`
-  )
+  ) */
 
   await driver.close()
 })();
-
-
-app.use(router.routes());
-app.use(router.allowedMethods());
-
-app.listen({port});
-console.log(`Server is running on port ${port}`);
