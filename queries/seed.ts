e28bd08949a84df1ac6156eba631ce3e516c8028
Jason Warren <contact.jwarren@gmail.com>
2025-01-24 18:04:26 +0000

    feat: :label: create interfaces for input & processed statements

--- queries/seed.ts
+++ queries/seed.ts
@@ -1,168 +1,185 @@
+// deno-lint-ignore-file ban-ts-comment
 import * as dotenv from "jsr:@std/dotenv";
-await dotenv.load({export: true});
-
 import neo4j, { Driver } from "neo4j";
 
+await dotenv.load({ export: true });
+
 const people = [
-  { name: "Alex",
+  {
+    name: "Alex",
     worksFor: ["Dan"],
     worksWith: ["Jason"],
     species: "Human",
-    enjoys: []
+    enjoys: [],
   },
-  { name: "Alphonso",
+  {
+    name: "Alphonso",
     worksFor: ["Biela", "Islington Council"],
     worksWith: ["Nich"],
     species: "Human",
-    enjoys: []
+    enjoys: [],
   },
-  { name: "Anna",
+  {
+    name: "Anna",
     worksFor: ["Dan"],
     worksWith: ["Jess"],
     species: "Human",
-    enjoys: []
+    enjoys: [],
   },
-  { name: "Biela",
+  {
+    name: "Biela",
     worksFor: [],
     worksWith: ["Jess"],
     species: "Dog",
-    enjoys: []
+    enjoys: [],
   },
-  { name: "Dan",
+  { name: "Dan", worksFor: [], worksWith: [], species: "Human", enjoys: [] },
+  {
+    name: "Harriet",
     worksFor: [],
     worksWith: [],
     species: "Human",
-    enjoys: []
+    enjoys: ["blanket", "treat"],
   },
-  { name: "Harriet",
-    worksFor: [],
-    worksWith: [],
-    species: "Human",
-    enjoys: ["blanket", "treat"]
-  },
-  { name: "Jack",
+  {
+    name: "Jack",
     worksFor: ["Dan"],
     worksWith: ["Max"],
     species: "Human",
-    enjoys: []
+    enjoys: [],
   },
-  { name: "Jason",
+  {
+    name: "Jason",
     worksFor: ["Dan"],
     worksWith: ["Alex"],
     species: "Goblin",
-    enjoys: []
+    enjoys: [],
   },
-  { name: "Jess",
+  {
+    name: "Jess",
     worksFor: ["Dan"],
     worksWith: ["Biela", "Anna"],
     species: "Human",
-    enjoys: []
+    enjoys: [],
   },
-  { name: "Max",
+  {
+    name: "Max",
     worksFor: ["Dan"],
     worksWith: ["Jack"],
     species: "Human",
-    enjoys: []
+    enjoys: [],
   },
-  { name: "Nich",
+  {
+    name: "Nich",
     worksFor: ["Dan"],
     worksWith: [],
     species: "Human",
-    enjoys: []
+    enjoys: [],
   },
-  { name: "Shaughn",
+  {
+    name: "Shaughn",
     worksFor: ["Dan"],
     worksWith: [],
     species: "Human",
-    enjoys: []
+    enjoys: [],
   },
-]
+];
 
 const items = [
   { name: "blanket" },
-  { name: "treat" }
-]
+  { name: "treat" },
+];
 
 const testSentence = [
   "Harriet enjoys blankets",
-  "Harriet is partial to a treat"
-]
+  "Harriet is partial to a treat",
+];
 
 export async function seed() {
-  const URI:string = await Deno.env.get("NEO4J_URI") ?? "";
-  const USER:string = await Deno.env.get("NEO4J_USERNAME") ?? "";
-  const PASSWORD:string = await Deno.env.get("NEO4J_PASSWORD") ?? "";
+  const URI: string = await Deno.env.get("NEO4J_URI") ?? "";
+  const USER: string = await Deno.env.get("NEO4J_USERNAME") ?? "";
+  const PASSWORD: string = await Deno.env.get("NEO4J_PASSWORD") ?? "";
 
+  // deno-lint-ignore prefer-const
   let driver: Driver, result;
 
   try {
-    driver = neo4j.driver(URI,  neo4j.auth.basic(USER, PASSWORD))
-    await driver.verifyConnectivity()
-  } catch(err) {
+    driver = neo4j.driver(URI, neo4j.auth.basic(USER, PASSWORD));
+    await driver.verifyConnectivity();
+  } catch (err) {
+    // @ts-ignore
     console.log(`Connection error\n${err}\nCause: ${err.cause}`);
-    return
+    return;
   }
 
-  for(const person of people) {
+  for (const person of people) {
     await driver.executeQuery(
-      'MERGE (p:Person {name: $person.name})',
+      "MERGE (p:Person {name: $person.name})",
       { person: person },
-      { database: 'neo4j' }
-    )
+      { database: "neo4j" },
+    );
   }
 
-  for(const item of items) {
+  for (const item of items) {
     await driver.executeQuery(
-      'MERGE (i:Item {name: $item.name})',
+      "MERGE (i:Item {name: $item.name})",
       { item: item },
-      { database: 'neo4j' }
-    )
+      { database: "neo4j" },
+    );
   }
 
-  for(const person of people) {
-    if(person.worksFor != undefined) {
-      await driver.executeQuery(`
+  for (const person of people) {
+    if (person.worksFor != undefined) {
+      await driver.executeQuery(
+        `
         MATCH (p:Person {name: $person.name})
         UNWIND $person.worksFor AS bossName
         MATCH (boss:Person {name: bossName})
         MERGE (p)-[:WORKS_FOR]->(boss)
-        `, { person: person },
-        { database: 'neo4j' }
-      )
+        `,
+        { person: person },
+        { database: "neo4j" },
+      );
     }
 
-    if(person.worksWith != undefined) {
-      await driver.executeQuery(`
+    if (person.worksWith != undefined) {
+      await driver.executeQuery(
+        `
         MATCH (p:Person {name: $person.name})
         UNWIND $person.worksFor AS colleagueName
         MATCH (colleague:Person {name: colleagueName})
         MERGE (p)-[:WORKS_WITH]->(colleague)
-        `, { person: person },
-        { database: 'neo4j' }
-      )
+        `,
+        { person: person },
+        { database: "neo4j" },
+      );
     }
 
-    if(person.enjoys != undefined) {
-      await driver.executeQuery(`
+    if (person.enjoys != undefined) {
+      await driver.executeQuery(
+        `
         MATCH (p:Person {name: $person.name})
         UNWIND $person.enjoys AS funThing
         MATCH (i:Item {name: funThing})
         MERGE (p)-[:ENJOYS]->(i)
-        `, { person: person },
-        { database: 'neo4j' }
-      )
+        `,
+        { person: person },
+        { database: "neo4j" },
+      );
     }
   }
-  
-  result = await driver.executeQuery(`
+
+  result = await driver.executeQuery(
+    `
     MATCH (p:Person)-[:WORKS_FOR]-(boss:Person)
     WHERE boss.species = $species
     RETURN boss
-    `, { species: "Dog" },
-    { database: 'neo4j' }
-  )
+    `,
+    { species: "Dog" },
+    { database: "neo4j" },
+  );
 
-  /* 
+  /*
     // Loop through results and do something with them
     for(let person of result.records) {
       // `person.friend` is an object of type `Node`
@@ -178,6 +195,6 @@ export async function seed() {
   */
 
   await driver.close();
-};
+}
 
 seed();
