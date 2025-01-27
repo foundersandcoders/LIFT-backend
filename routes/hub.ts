63088fa4d5f14204136e85dcdd8ff19dc3554a19
Jason Warren <contact.jwarren@gmail.com>
2025-01-27 18:22:47 +0000

    feat: :monocle_face: allow retrieving all nodes

--- routes/hub.ts
+++ routes/hub.ts
@@ -1,15 +1,51 @@
 import { Router } from "acorn";
 import { EntryInput } from "types";
+import { queryAll } from "../queries/queryAll.ts";
 
 const router = new Router();
 
-// ALEX
-// The endpoint you want is "/newEntry"
-// The type definitions are in "../types/input.ts"
+router.get("/all", async (ctx) => {
+  try {
+    const records = await queryAll();
+    
+    if (!records) { return {
+      status: 500,
+      body: { error: "Failed to fetch records from the database" },
+    }}
 
-router.get("/", () => ({
-  query: `( ??? )`,
-}));
+    return {
+      status: 200,
+      body: records,
+    };
+  } catch (error) {
+    console.error("Error fetching data:", error);
+
+    return {
+      status: 500,
+      body: { error: "Internal Server Error" },
+    };
+  }
+});
+
+/* Get By Part of Speech
+  router.get("/subject/:name", (ctx) => {
+    return {
+      query: `( ${ctx.params.name} )---[ ??? ]-->( ??? )`,
+    };
+  });
+
+  router.get("/object/:name", (ctx) => {
+    return {
+      query: `( ??? )---[ ??? ]-->( ${ctx.params.name} )`,
+    };
+  });
+
+  router.get("/verb/:name", (ctx) => {
+    return {
+      query: `( ??? )---[ ${ctx.params.name} ]--> ( ??? )`,
+    };
+  });
+*/
 
 router.post("/newEntry", async (ctx) => {
   try {
@@ -21,43 +57,19 @@ router.post("/newEntry", async (ctx) => {
       || !entry.verb
       || !entry.object
       || typeof entry.isPublic !== "boolean"
-    ) {
-      throw new Error("Missing or invalid required fields");
-    }
+    ) { throw new Error("Missing or invalid required fields") }
 
     console.log("Received new entry:", entry);
 
-    return {
-      message: "Entry received successfully",
-    };
+    return { message: "Entry received successfully" };
   } catch (error) {
     console.error("Error processing entry:", error);
 
     return {
       status: 400,
-      body: {
-        error: "Invalid input format",
-      },
+      body: {error: "Invalid input format"}
     };
   }
-});
-
-router.get("/subject/:name", (ctx) => {
-  return {
-    query: `( ${ctx.params.name} )---[ ??? ]-->( ??? )`,
-  };
-});
-
-router.get("/object/:name", (ctx) => {
-  return {
-    query: `( ??? )---[ ??? ]-->( ${ctx.params.name} )`,
-  };
-});
-
-router.get("/verb/:name", (ctx) => {
-  return {
-    query: `( ??? )---[ ${ctx.params.name} ]--> ( ??? )`,
-  };
 });
 
 export default router;
