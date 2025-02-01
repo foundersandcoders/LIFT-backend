c72f1b420ec985eec5a869918ad720ad6f338967
Jason Warren <contact.jwarren@gmail.com>
2025-02-01 23:02:12 +0000

    feat: :rocket: use cron to schedule DB nudge

---
+++ utils/nudgeDb.ts
@@ -0,0 +1,25 @@
+import neo4j, { Driver } from "neo4j";
+import { creds as c } from "./creds/neo4j.ts";
+import { nextDate } from "jsr:@coven/cron";
+
+export const nudgeSched = "0 */4 * * *"; // Once every 4 hours
+const nextRun = () => { nextDate(new Date())(nudgeSched) };
+
+export async function nudgeDb () {
+    let driver: Driver;
+    console.groupCollapsed("=== DB Nudger ===");
+
+    console.log(`Opening driver`);
+    try {
+        driver = neo4j.driver(c.URI, neo4j.auth.basic(c.USER, c.PASSWORD));
+        console.log(`Nudge successful`);
+    } catch (err) {
+        console.error("Nudge failed:", err);
+    } finally {
+        await driver.close();
+        console.log(`Driver closed`);
+        console.log(`Next run: ${nextRun}`)
+    }
+
+    console.groupEnd();
+};
